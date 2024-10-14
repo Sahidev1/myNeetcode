f09aabb777d71ab5c76cc09e74609b279155246b
Sahidev1 <theoddrogue@gmail.com>
2024-10-14 15:04:23 +0200

    updated best time to buy and sell

--- Easy_problems/best_time_to_buy_and_sell_stock/python/solution.py
+++ Easy_problems/best_time_to_buy_and_sell_stock/python/solution.py
@@ -1,15 +1,17 @@
 class Solution:
     def maxProfit(self, prices: list[int]) -> int:
         maxP:int = 0
-        baseI:int = 0
         arrLen:int = len(prices)
+        lPtr:int = 0
+        rPtr:int = lPtr + 1
+        lowest = prices[lPtr]
 
-        while baseI < arrLen - 1:
-            nextI = baseI + 1
-            while nextI < arrLen:
-                currProf = prices[nextI] - prices[baseI]
-                if currProf > maxP: maxP = currProf
-                nextI += 1
-            baseI += 1
+        while rPtr < arrLen:
+            if prices[lPtr] < lowest: lowest = prices[lPtr]
+            currP = prices[rPtr] - lowest
+            if currP > maxP: maxP = currP
+            lPtr = rPtr
+            rPtr += 1
+
         
         return maxP
