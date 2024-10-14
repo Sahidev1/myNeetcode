63f91f5537e38146164b4ba5039332c1b375ed97
Sahidev1 <theoddrogue@gmail.com>
2024-10-14 16:52:51 +0200

    reverse linked list solved

--- Easy_problems/main.java
+++ Easy_problems/main.java
@@ -1,8 +1,31 @@
-import valid_parantheses.java.Solution;
-
+import reverse_linked_list.java.Solution;
+import reverse_linked_list.java.Solution.ListNode;
 class Main {
     public static void main(String[] args) {
         Solution sol = new Solution();
-        System.out.println(sol.isValid("]"));
+        int[] vals = {0,1,2,3};
+        ListNode l = arrToListNode(vals, sol);
+        listNodeIterator(l);
+        l=sol.reverseList(l);
+        System.out.println("reversed");
+        listNodeIterator(l);
+
+    }
+
+    private static ListNode arrToListNode(int[] arr, Solution sol){
+        ListNode root = sol.new ListNode(arr[0], null);
+        ListNode curr = root;
+        for(int i = 1; i < arr.length; i++){
+            curr.next = sol.new ListNode(arr[i],null);
+            curr = curr.next;
+        }
+        return root;
+    }
+
+    private static void listNodeIterator(ListNode root){
+        while(root != null){
+            System.out.println(root.val);
+            root = root.next;
+        }
     }
 }
---
+++ Easy_problems/reverse_linked_list/java/Solution.java
@@ -0,0 +1,40 @@
+package reverse_linked_list.java;
+
+
+public class Solution {
+    public class ListNode {
+        public int val;
+        public ListNode next;
+        public ListNode() {}    ListNode(int val) { this.val = val; }
+        public ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+      }
+
+    public ListNode reverseList(ListNode head) {
+        ListNode curr = head;
+        ListNode saveNext;
+        ListNode newNext = null;
+
+        while(true){
+            if(curr == null){
+                head = newNext;
+                break;
+            }
+            saveNext = curr.next;
+
+            curr.next = newNext;
+            newNext = curr;
+            curr = saveNext;
+        }
+
+        return head;
+    }
+
+    private void iterator(ListNode root){
+        System.out.print("[");
+        while(root != null){
+            System.out.print(root.val+", ");
+            root = root.next;
+        }
+        System.out.println("]");
+    }
+}
