aee666b9435f07680a0a8fb9455c10b8b3d11ea5
Sahidev1 <theoddrogue@gmail.com>
2024-11-01 23:53:17 +0100

    climbing stairs solved, not optimal though

---
+++ Easy_problems/climbing_stairs/python/Solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        self.mem:dict = {}
+        return self.climb(n)
+
+
+    def climb(self, n:int)->int:
+        if n <= 3: return n
+        remember = self.mem.get(n)
+        if remember != None: return remember
+        climb2 = self.climbStairs(n - 2)
+        climb1 = self.climbStairs(n - 1)
+        self.mem[n] = climb1 + climb2
+        return climb1 + climb2
