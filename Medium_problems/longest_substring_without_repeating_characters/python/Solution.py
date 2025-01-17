8483f87cdf507fc579c59ee5cf1f3688e9545ab6
Sahidev1 <theoddrogue@gmail.com>
2025-01-17 17:42:10 +0100

    optimized solution created

--- Medium_problems/longest_substring_without_repeating_characters/python/Solution.py
+++ Medium_problems/longest_substring_without_repeating_characters/python/Solution.py
@@ -1,7 +1,11 @@
 
 
+import time
+
+
 class Solution:
-    def lengthOfLongestSubstring(self, s: str) -> int:
+    #complexity is actually approximately O(n²)
+    def lengthOfLongestSubstringUnoptimized(self, s: str) -> int:
         bag = set()
         i = 0
         clist = list(s)
@@ -19,7 +23,35 @@ class Solution:
 
         return maxW
 
-sol = Solution()
-s="pwwwkew"
+    #optimized solution 
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        bag = set()
+        b = 0 
+        t = 0
+        clist = s
+        wmax = 0
 
+        while t < len(clist):
+            while t < len(clist) and clist[t] not in bag:
+                bag.add(clist[t])
+                t += 1
+                
+            wmax = max(t - b, wmax)                
+            bag.discard(clist[b])
+            b += 1
+        
+        return wmax
+
+
+
+sol = Solution()
+
+s="pwwkew"
+
+start_time = time.time_ns()
+print(sol.lengthOfLongestSubstringUnoptimized(s)) # 4
+print("Time elapsed: ", time.time_ns() - start_time)
+
+start_time = time.time_ns()
 print(sol.lengthOfLongestSubstring(s)) # 4
+print("Time elapsed: ", time.time_ns() - start_time)
