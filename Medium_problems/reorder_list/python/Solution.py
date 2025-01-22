e2ba076c92f881a3c5ec5fcbf885fc3c565a3828
Sahidev1 <theoddrogue@gmail.com>
2025-01-22 17:58:26 +0100

    reorder list optimized O(n) solution

--- Medium_problems/reorder_list/python/Solution.py
+++ Medium_problems/reorder_list/python/Solution.py
@@ -8,58 +8,96 @@ class ListNode:
          self.val = val
          self.next = next
 
-def generateLL(aar: list[int]) -> Optional[ListNode]:
-    head = ListNode(aar[0])
+def arrayToList(arr):
+    head = ListNode(arr[0])
     current = head
-    for i in range(1, len(aar)):
-        current.next = ListNode(aar[i])
+    for i in range(1, len(arr)):
+        current.next = ListNode(arr[i])
         current = current.next
+
     return head
 
-def printLL(head: Optional[ListNode]):
-    while head != None:
-        print(head.val)
-        head = head.next
+def ListToArray(head):
+    arr = []
+    current = head
+    while current != None:
+        arr.append(current.val)
+        current = current.next
 
+    return arr
+
+printList = lambda head: print(ListToArray(head))
 
 class Solution:
-    def getLLSize(self, head: Optional[ListNode]) -> int:
+    def getMiddle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        s, f = head, head # slow, fast
         size = 0
-        while head != None:
-            size += 1
-            head = head.next
-        return size
+        prevs = head
+        while f != None and f.next != None:
+            prevs = s
+            s = s.next
+            f = f.next.next
+            size+=2
+        if f is not None: size+=1
+        
+        if size % 2 == 0: s = prevs
 
-    def moveSteps(self, start: Optional[ListNode], steps:int):
-        while steps > 0:
-            start = start.next
-            steps -= 1
+        return s
 
-        return start
-    
+    def createSecondList(self, mid)-> tuple[Optional[ListNode], Optional[ListNode]]:
+        if mid.next is None:
+            return None
+        else: 
+            nxt = mid.next
+            mid.next = None
+            return nxt
 
+    def reverseList(self, head: Optional[ListNode])-> Optional[ListNode]:
+        prev, curr = None, head
+        while curr is not None:
+            saveNext = curr.next
+            curr.next = prev
+            prev = curr
+            curr = saveNext
+
+        return prev
+        
+
+    def mergeLists(self, base: Optional[ListNode], appending: Optional[ListNode]) -> Optional[ListNode]:
+        l1, l2 = base, appending
+
+        while l1 is not None and l2 is not None:
+            l1next = l1.next
+            l2next = l2.next
+            l1.next = l2
+            l2.next = l1next
+            l1, l2 = l1next, l2next
 
     def reorderList(self, head: Optional[ListNode]) -> None:
-        nSize:int = self.getLLSize(head)
-        steps = nSize - 1
-        f = head
-
-        print(f'List size: {nSize}, steps: {steps}')
-
-        while steps > 0:
-            n = f.next
-            last = self.moveSteps(f, steps)
-            print(f'f: {f.val}, last: {last.val}, n: {n.val}')
-            f.next = last 
-            last.next = n
-            f = n
-            steps -= 2
-        
-        f.next = None
+        mid = self.getMiddle(head)
+        l2 = self.createSecondList(mid)
+        l2=self.reverseList(l2)
+        self.mergeLists(head, l2)
 
 
-llist = generateLL([1,2,3,4,5,6])
-#printLL(llist)
-sol = Solution()
-sol.reorderList(llist)
-printLL(llist)
+def test(arr: list[int]):
+    head = arrayToList(arr)
+    printList(head)
+    sol = Solution()
+    sol.reorderList(head)
+    printList(head)
+
+testCases: list[list[int]] = [
+    range(0,1),
+    range(0,2),
+    range(0,3),
+    range(0,4),
+    range(0,5),
+    range(0,6),
+]
+
+for test_case in testCases:
+    print("Test case nr: ", testCases.index(test_case))
+    test(test_case)
+    print("\n\n")
+
